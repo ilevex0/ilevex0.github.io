5f92b5d48f4793e49038c1b432f229c56d3e71c4
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-08-07 15:43:45 -0300

    Fix thisWebSite Bugs

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ async function getProjectsAPI() {
     projects.forEach(project => {
         verifyIfHasImage(project.name, (exists) => {
             if (exists) {
-                console.log('A imagem existe.');
+                //console.log('A imagem existe.');
                 let elemento = document.querySelector(`#${project.name}`);
                 if(elemento != null){
                     elemento.src = `./img/${project.name}.png`
@@ -21,4 +21,12 @@ async function getProjectsAPI() {
             }
         });
     })
+    //adiciona alterações no projeto do site do portfólio
+    const thisWebsite = document.getElementById(`ilevex0.github.io`);
+    thisWebsite.src = `./img/thiswebsite.png`;
+    const thisWebsitePageLink = document.querySelector("a[href='https://ilevex0.github.io/']");
+    const thisWebsiteButtonLink = document.querySelector("a.projects__container__link[href='https://ilevex0.github.io/']");
+    thisWebsitePageLink.href = "https://github.com/ilevex0/ilevex0.github.io";
+    thisWebsiteButtonLink.href = "https://github.com/ilevex0/ilevex0.github.io";
+
 }
